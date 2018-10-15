c66a1968b7036a1ed1a6f96f84ca16885e9eccac
Adam Coyle <adam.coyle@fhnw.ch>
2018-10-15 16:09:06 +0200

    Added a basic Space Monster and a Basic Score Counter

---
+++ Assets/MonsterObstacle.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace Rocket
+{
+    public class MonsterObstacle : MonoBehaviour
+    {
+        Rigidbody2D monsterRB;
+
+        float speed = 100.0f;
+
+        bool movingRight;
+
+        // Use this for initialization
+        void Start()
+        {
+            movingRight = true;
+            monsterRB = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void FixedUpdate()
+        {
+            MoveMonster();
+            
+        }
+
+        private void MoveMonster()
+        {
+            if (movingRight)
+            {
+                monsterRB.velocity = Vector2.right * speed * Time.deltaTime;
+            }
+            else
+            {
+                monsterRB.velocity = Vector2.left * speed * Time.deltaTime;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            RocketHealth rocketHealth = collision.GetComponent<RocketHealth>();
+            if (rocketHealth)
+            {
+                rocketHealth.DestroyRocket();
+            }
+
+            if (collision.GetComponent<ScreenEdge>())
+            {
+                movingRight = !movingRight;
+            }
+        }
+    } 
+}
---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Rocket
+{
+    public class ScoreManager : MonoBehaviour
+    {
+        [SerializeField]
+        Text scoreText;
+
+        float score;
+
+        int multiplier = 60;
+
+        // Use this for initialization
+        void Start()
+        {
+            score = 0;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            score += Time.deltaTime;
+            scoreText.text = score.ToString();
+            Debug.Log(score);
+        }
+    } 
+}
