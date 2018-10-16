d56add16cec4fd6d13d6c2218cef9ac3849464ba
Adam Coyle <adam.coyle@fhnw.ch>
2018-10-16 16:22:22 +0200

    Pause Button Works

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,6 +5,15 @@ namespace Rocket
 {
     public class LevelManager : MonoBehaviour
     {
+        public void PauseGame()
+        {
+            Time.timeScale = 0;
+        }
+
+        public void UnpauseGame()
+        {
+            Time.timeScale = 1;
+        }
 
         public void LoadLevel(string name)
         {
