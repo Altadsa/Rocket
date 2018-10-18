2c044948acae2bb3ba1459d70445bb6880938f89
Adam Coyle <adam.coyle@fhnw.ch>
2018-10-18 14:46:24 +0200

    Started on the EDT Item, but it currently breaks the game by not allowing the rocket to trigger the Spawn Areas.

---
+++ Assets/Scripts/EDT.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rocket
+{
+    public class EDT : MonoBehaviour
+    {
+
+        float edtDuration = 10.0f;
+
+        // Use this for initialization
+        void Start()
+        {
+            Rocket rocket = GetComponentInParent<Rocket>();
+            if (!rocket) { return; }
+
+            StartCoroutine(TransferToEtherealDimension(edtDuration));
+        }
+
+        IEnumerator TransferToEtherealDimension(float duration)
+        {
+            Rocket rocket = GetComponentInParent<Rocket>();
+            if (rocket)
+            {
+                rocket.GetComponent<PolygonCollider2D>().enabled = false;
+                yield return new WaitForSeconds(duration);
+                rocket.GetComponent<PolygonCollider2D>().enabled = true;
+            }
+        }
+    } 
+}
